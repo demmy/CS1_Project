43222c0610932cccb9c8b0ee8037b014c27afd39
SalamanderSunburn <taisiya.loza@gmail.com>
2015-08-01 23:03:55 +0300

    anchor added

--- Solution/ContosoProject/ContosoUI/OrderSearchForm/OrderSearchForm.Designer.cs
+++ Solution/ContosoProject/ContosoUI/OrderSearchForm/OrderSearchForm.Designer.cs
@@ -93,6 +93,9 @@
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.layoutControl1.Controls.Add(this.panelControl3);
             this.layoutControl1.Controls.Add(this.StatusComboBoxEdit);
             this.layoutControl1.Controls.Add(this.OrderNumberTextEdit);
--- Solution/ContosoProject/ContosoUI/ProductSearchForm/ProductSearchForm.Designer.cs
+++ Solution/ContosoProject/ContosoUI/ProductSearchForm/ProductSearchForm.Designer.cs
@@ -124,6 +124,9 @@
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.layoutControl1.Controls.Add(this.textEdit3);
             this.layoutControl1.Controls.Add(this.textEdit2);
             this.layoutControl1.Controls.Add(this.TitleLabelControl);
