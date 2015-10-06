0c1a23ad7b7c36418c39b49849e50427a3956b10
imnikoo <efruev@gmail.com>
2015-10-06 21:10:46 +0300

    Part of entities
    Entity, ExtendedEntity, Permission, Role, User

---
+++ Solution/ContosoProject/Domain/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Entity
+    {
+        public int ID { get; private set; }
+        public bool IsActive { get; set; }
+
+        public Entity(int id, bool isActive)
+        {
+            ID = id;
+            IsActive = isActive;
+        }
+    }
+}
