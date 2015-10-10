13f0a9e256d0b557416acd5513dde162704dc5ed
SalamanderSunburn <taisiya.loza@gmail.com>
2015-10-11 00:09:30 +0300

    IRepo commit

---
+++ Solution/ContosoProject/Domain/DAO/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Clients;
+
+namespace Domain.DAO
+{
+    public interface IClientRepository : IRepository<Client>
+    {
+        ICollection<Client> GetByName(string name);
+        ICollection<Client> GetByCity(string city);
+    }
+}
---
+++ Solution/ContosoProject/Domain/DAO/IPermissionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Users;
+
+namespace Domain.DAO
+{
+    public interface IPermissionRepository : IRepository<Permission>
+    {
+        ICollection<Permission> GetByTitle(string title);
+    }
+}
