1f022fe798986a5931b9952fe76c6b7330a47e1d
SalamanderSunburn <taisiya.loza@gmail.com>
2015-10-11 15:47:17 +0300

    commit

---
+++ Solution/ContosoProject/Domain/Entities/ExtendedEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain.Entities.Users;
+
+namespace Domain.Entities
+{
+    public abstract class ExtendedEntity : Entity
+    {
+        protected ExtendedEntity()
+        {
+        }
+
+        public User User { get; set; }
+
+        public DateTime Date { get; set; }
+
+     } 
+}
---
+++ Solution/ContosoProject/Domain/Entities/Users/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities.Users
+{
+    public class User : ExtendedEntity
+    {
+        public User()
+        {
+            
+        }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
