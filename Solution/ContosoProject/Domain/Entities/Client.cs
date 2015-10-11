db808763c993761944a5425fe4bcb4792e4374fa
imnikoo <efruev@gmail.com>
2015-10-11 16:14:10 +0300

    Entitties

--- Solution/ContosoProject/Domain/Entities/Client.cs
+++ Solution/ContosoProject/Domain/Entities/Client.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Client
+    public class Client : ExtendedEntity
     {
+        Location ClientLocation { get; set; }
+        private ICollection<string> _telephones;
+
+
+        public Client(ICollection<string> telephones, DateTime date, bool isActive)
+            : base(date, isActive)
+        {
+            _telephones = telephones;
+        }
+
+
+        public IReadOnlyCollection<string> Telephones
+        {
+            get
+            {
+                return (IReadOnlyCollection<string>)_telephones;
+            }
+        }
+
+
+        protected bool Equals(Client other)
+        {
+            return Equals(_telephones, other._telephones) && Equals(ClientLocation, other.ClientLocation);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Client)obj);
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((_telephones != null ? _telephones.GetHashCode() : 0) * 397) ^ (ClientLocation != null ? ClientLocation.GetHashCode() : 0);
+            }
+        }
     }
+
 }
---
+++ Solution/ContosoProject/Domain/Entities/Location.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Location
+    {
+        public string City { get; set; }
+        public string Adress { get; set; }
+
+        public Location()
+        {
+
+        }
+
+        protected bool Equals(Location other)
+        {
+            return string.Equals(City, other.City) && string.Equals(Adress, other.Adress);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Location)obj);
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((City != null ? City.GetHashCode() : 0) * 397) ^ (Adress != null ? Adress.GetHashCode() : 0);
+            }
+        }
+    }
+
+}
