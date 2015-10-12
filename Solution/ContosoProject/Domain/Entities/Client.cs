ce9d79679d0be44a10b4d99809466343de82cca4
imnikoo <efruev@gmail.com>
2015-10-12 23:52:34 +0300

    person

--- Solution/ContosoProject/Domain/Entities/Client.cs
+++ Solution/ContosoProject/Domain/Entities/Client.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities
     public class Client : ExtendedEntity
     {
         public Location ClientLocation { get; set; }
+        public Person Person { get; set; }
         private ICollection<string> _telephones;
         public Client(ICollection<string> telephones)
         {
