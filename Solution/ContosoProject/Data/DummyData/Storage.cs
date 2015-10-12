8d879379efe102b398167357856938932fff40f1
Vaillant <matvienko.serega2012@gmail.com>
2015-10-12 16:58:42 +0300

    New part of Dummy datas
    Clients, Locations, Products, Categories

--- Solution/ContosoProject/Data/DummyData/Storage.cs
+++ Solution/ContosoProject/Data/DummyData/Storage.cs
@@ -1,103 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Domain.Entities.Comments;
 using Domain.Entities.Users;
 
 namespace Data.DummyData
 {
-    class PermissionDictionary:KeyedCollection<string, Permission>
+    static partial class Storage
     {
-        protected override string GetKeyForItem(Permission item)
-        {
-            return item.Title;
-        }
-    }
-    static class Storage
-    {
-        #region Permissions
-
-        private static readonly PermissionDictionary Permissions = new PermissionDictionary
-        {
-            new Permission {Description = "", Id = 1, IsActive = true, Title = "Add User"},
-            new Permission {Description = "", Id = 2, IsActive = true, Title = "Edit User"},
-            new Permission {Description = "", Id = 3, IsActive = true, Title = "Activate User"},
-            new Permission {Description = "", Id = 4, IsActive = true, Title = "Deactivate User"},
-
-            new Permission {Description = "", Id = 5, IsActive = true, Title = "Add Product"},
-            new Permission {Description = "", Id = 6, IsActive = true, Title = "Edit Product"},
-            new Permission {Description = "", Id = 7, IsActive = true, Title = "Activate Product"},
-            new Permission {Description = "", Id = 8, IsActive = true, Title = "Deactivate Product"},
-
-            new Permission {Description = "", Id = 9, IsActive = true, Title = "Add Client"},
-            new Permission {Description = "", Id = 10, IsActive = true, Title = "Edit Client"},
-
-            new Permission {Description = "", Id = 11, IsActive = true, Title = "Add Comment"},
-            new Permission {Description = "", Id = 12, IsActive = true, Title = "Edit Comment"},
-
-            new Permission {Description = "", Id = 13, IsActive = true, Title = "Add Order"},
-            new Permission {Description = "", Id = 14, IsActive = true, Title = "Edit Order"},
-            new Permission {Description = "", Id = 15, IsActive = true, Title = "Activate Order"},
-            new Permission {Description = "", Id = 16, IsActive = true, Title = "Deactivate Order"}
-        };
-
-        private static readonly List<Permission> AdminPermissions = new List<Permission>
-        {
-            Permissions["Add User"],
-            Permissions["Edit User"],
-            Permissions["Activate User"],
-            Permissions["Deactivate User"],
-            Permissions["Add Product"],
-            Permissions["Edit Product"],
-            Permissions["Activate Product"],
-            Permissions["Deactivate Product"],
-            Permissions["Add Client"],
-            Permissions["Edit Client"],
-            Permissions["Add Comment"],
-            Permissions["Edit Comment"],
-            Permissions["Add Order"],
-            Permissions["Edit Order"],
-            Permissions["Activate Order"],
-            Permissions["Deactivate order"]
-        };
-
-        private static readonly List<Permission> DirectorPermissions = new List<Permission>
-        {
-            Permissions["Add Product"],
-            Permissions["Edit Product"],
-            Permissions["Activate Product"],
-            Permissions["Deactivate Product"],
-            Permissions["Add Client"],
-            Permissions["Edit Client"],
-            Permissions["Add Comment"],
-            Permissions["Edit Comment"],
-            Permissions["Add Order"],
-            Permissions["Edit Order"],
-            Permissions["Activate Order"],
-            Permissions["Deactivate order"]
-        };
-
-        private static readonly List<Permission> ManagerPermissions = new List<Permission>
-        {
-            Permissions["Add Client"],
-            Permissions["Edit Client"],
-            Permissions["Add Comment"],
-            Permissions["Edit Comment"],
-            Permissions["Add Order"],
-            Permissions["Edit Order"],
-            Permissions["Activate Order"],
-            Permissions["Deactivate order"]
-        };
-
-        #endregion
-        
-        static readonly List<Comment> GodComments = new List<Comment>
+        public static List<Comment> GodComments = new List<Comment>
         {
             new Comment{Text = "the best man ever"},
             new Comment{Text = "Good guy, you can believe him in any case"},
             new Comment{Text = "Don't Troube him too much, he doesn't like this"},
             new Comment{Text = "Gon't try to kick him"}
-        }; 
+        };
 
         public static List<User> Users = new List<User>
         {
