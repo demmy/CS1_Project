7297a3b21ead2d17f731e56fb85318d2c31513b7
SalamanderSunburn <taisiya.loza@gmail.com>
2015-10-15 16:40:42 +0300

    merge with imnikoo-UI

--- Solution/ContosoProject/ContosoUI/Program.cs
+++ Solution/ContosoProject/ContosoUI/Program.cs
@@ -16,7 +16,7 @@ namespace ContosoUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new ClientForm(null));
         }
     }
 }
