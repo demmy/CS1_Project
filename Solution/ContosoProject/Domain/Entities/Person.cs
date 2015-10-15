aa889ae29c6392c81c08d926cbbabb8863c43f40
imnikoo <efruev@gmail.com>
2015-10-15 15:54:18 +0300

    person: equals, gethashcode impl

--- Solution/ContosoProject/Domain/Entities/Person.cs
+++ Solution/ContosoProject/Domain/Entities/Person.cs
@@ -1,1 +1,37 @@
-﻿using System;  namespace Domain.Entities {     public class Person     {         public string FirstName { get; set; }         public string MiddleName { get; set; }         public string LastName { get; set; }          public Person()         {          }     } }
+﻿using System;  namespace Domain.Entities {     public class Person     {
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Person)obj);
+        }
+
+        protected bool Equals(Person other)
+        {
+            return string.Equals(FirstName, other.FirstName) && string.Equals(MiddleName, other.MiddleName) && string.Equals(LastName, other.LastName);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (FirstName != null ? FirstName.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (MiddleName != null ? MiddleName.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (LastName != null ? LastName.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        
+    } }
