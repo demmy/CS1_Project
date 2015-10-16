be004b926cd13f2f1392703814086eb582985944
imnikoo <efruev@gmail.com>
2015-10-16 12:25:04 +0300

    test

--- Solution/ContosoProject/Test/NUNitDAOQuerryTests.cs
+++ Solution/ContosoProject/Test/NUNitDAOQuerryTests.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Data.DummyData;
 using Data.StoreData;
 using Domain.Entities;
+using Domain.Entities.Orders;
+using Domain.Entities.Users;
 using NUnit.Framework;
 
 namespace Test
@@ -19,27 +21,50 @@ namespace Test
         {
             DummyDAOForCategory category = new DummyDAOForCategory();
         }
-
-        [TestCase(Conver. person, )]
+        [Test]
+        public void TestDAOForClientGetByCity()
+        {
+            DummyDAOForClient client = new DummyDAOForClient();
+            CollectionAssert.AreEqual(Storage.Clients, client.GetByCity("Dnepropetrovsk"));
+        }
+        [Test]
         public void TestDAOForClientGetByPerson()
         {
             DummyDAOForClient client = new DummyDAOForClient();
-            CollectionAssert.AreEqual(new List<Client>() { Storage.Clients[2] }, client.GetByPerson(Storage.Persons[8]));
+            CollectionAssert.AreEqual(new List<Client> {Storage.Clients[2] }, client.GetByPerson(Storage.Persons[8]));
         }
+
         [Test]
         public void TestDAOForComment()
         {
 
         }
         [Test]
-        public void TestDAOForOrder()
+        public void TestDAOForOrderGetByClient()
         {
-
+            DummyDAOForOrder order = new DummyDAOForOrder();
+            CollectionAssert.AreEqual(new List<Order> { Storage.Orders[0] },order.GetByClient(Storage.Clients[0]));
+            Assert.Throws<Exception>(delegate { order.GetByStatus(Status.Shipped); } );
         }
+        [Test]
+        public void TestDAOForOrderGetByProduct()
+        {
+            DummyDAOForOrder order = new DummyDAOForOrder();
+            CollectionAssert.AreEqual(new List<Order> { Storage.Orders[0], Storage.Orders[1] }, order.GetByProduct(Storage.Products[0]));
+        }
+
+        [Test]
+        public void TestDAOForOrderGetByStatus()
+        {
+            DummyDAOForOrder order = new DummyDAOForOrder();
+            CollectionAssert.AreEqual(new List<Order> { Storage.Orders[0], Storage.Orders[2]}, order.GetByStatus(Status.Opened));
+        }
+
         [Test]
         public void TestDAOForPermission()
         {
-
+            DummyDAOForPermission permission = new DummyDAOForPermission();
+            CollectionAssert.AreEqual(new List<Permission> { Storage.Permissions[8]}, "Add Client");
         }
         [Test]
         public void TestDAOForProduct()
