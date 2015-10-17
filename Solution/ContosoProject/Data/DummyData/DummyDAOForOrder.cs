1602ad0b9d6460d9919982e7d803d430544864a7
SalamanderSunburn <taisiya.loza@gmail.com>
2015-10-17 16:59:58 +0300

    merged data changes

--- Solution/ContosoProject/Data/DummyData/DummyDAOForOrder.cs
+++ Solution/ContosoProject/Data/DummyData/DummyDAOForOrder.cs
@@ -1,51 +1,87 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.StoreData;
 using Domain.DAO;
 using Domain.Entities;
+using Domain.Entities.Comments;
 using Domain.Entities.Orders;
 using Domain.Entities.Products;
 
 namespace Data.DummyData
 {
-    public class DummyDAOForOrder : DummyDAO<Order>, IOrderRepository
+    class DummyDAOForOrder : DummyDAOExtension<Order>, IOrderRepository
     {
+        public DummyDAOForOrder()
+        {
+            _collection = Storage.Orders;
+        }
         public ICollection<Order> GetByClient(Client client)
         {
-            throw new System.NotImplementedException();
+            if (_collection.Any(x => x.Client == client))
+            {
+                return _collection.Where(x => x.Client == client).ToList();
+            }
+            throw new Exception();
         }
 
         public ICollection<Order> GetByProduct(Product product)
         {
-            throw new System.NotImplementedException();
+            if (Contains(product))
+            {
+                return _collection.Where(x => x.OrderItems.Any(it => it.Product == product)).ToList();
+            }
+            throw new Exception();
         }
 
         public ICollection<Order> GetByStatus(Status status)
         {
-            throw new System.NotImplementedException();
+            if (_collection.Any(x => x.Status == status))
+            {
+                return _collection.Where(x => x.Status== status).ToList();
+            }
+            throw new Exception();
         }
 
         public void AddOrder(Product product, int quantity)
         {
-            throw new System.NotImplementedException();
+            if (!Contains(product))
+            {
+                _collection.Add(new Order(new List<Comment>(), new List<OrderItem>()
+                {
+                    new OrderItem(product, quantity, product.Price)
+                } ));
+            }
+            throw new Exception();
         }
 
         public bool Contains(Product product)
         {
-            throw new System.NotImplementedException();
+            return _collection.Any(x => x.OrderItems.Any(it => it.Product == product));
         }
 
         public void RemoveOrder(Product product)
         {
-            throw new System.NotImplementedException();
+            if (Contains(product))
+            {
+                _collection.First(x => x.OrderItems.Any(it => it.Product == product)).IsActive = false;
+            }
+            throw  new Exception();
         }
 
         public void EditOrder(Product product, int quantity)
         {
-            throw new System.NotImplementedException();
+            if (Contains(product))
+            {
+                var prod = _collection[_collection.IndexOf(_collection.First(x => x.OrderItems.Any(it => it.Product == product)))].OrderItems.First(x => x.Product == product);
+                prod.Quantity = quantity;
+            }
+            throw new Exception();
         }
 
         public double Sum
         {
-            get { throw new System.NotImplementedException(); }
+            get { return _collection.Sum(x => x.Sum); }
         }
     }
 }
--- Solution/ContosoProject/Data/DummyData/DummyDAOForUser.cs
+++ Solution/ContosoProject/Data/DummyData/DummyDAOForUser.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.StoreData;
 using Domain.DAO;
 using Domain.Entities.Users;
 
 namespace Data.DummyData
 {
-    public class DummyDAOForUser : DummyDAO<User>, IUserRepository
+    class DummyDAOForUser : DummyDAOExtension<User>, IUserRepository
     {
+        public DummyDAOForUser()
+        {
+            _collection = Storage.Users;
+        }
         public ICollection<User> GetByLogin(string login)
         {
-            throw new System.NotImplementedException();
+            if (_collection.Any(x => x.Login == login))
+            {
+                return _collection.Where(x=>x.Login==login).ToList();
+            }
+            throw new Exception();
         }
 
         public ICollection<User> GetByRole(Role role)
@@ -18,7 +29,11 @@ namespace Data.DummyData
 
         public ICollection<User> GetByName(string fn, string ln)
         {
-            throw new System.NotImplementedException();
+            if (_collection.Any(x => x.Role == role))
+            {
+                return _collection.Where(x => x.Role == role).ToList();
+            }
+            throw new Exception();
         }
     }
 }
