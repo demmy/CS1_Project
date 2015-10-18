92283f28bc571f0fa8b64094104579334c5cafbd
imnikoo <efruev@gmail.com>
2015-10-18 20:30:37 +0300

    implementaion for clients DAO

--- Solution/ContosoProject/Data/DummyData/DummyDAOForClient.cs
+++ Solution/ContosoProject/Data/DummyData/DummyDAOForClient.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Data.StoreData;
-using System.Threading.Tasks;
 using Domain.DAO;
 using Domain.Entities;
 
@@ -18,75 +16,24 @@ namespace Data.DummyData
 
         public ICollection<Client> FindBy(Person person, string city)
         {
-            switch (person.GetInformationAmount(city))
+            IQueryable<Client> result = new EnumerableQuery<Client>(_collection);
+            if (!string.IsNullOrWhiteSpace(person.FirstName))
             {
-                case PersonExtention.InformationAmount.City:
-                {
-                    return _collection.Where(x => x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstName:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameCity:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameLastName:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.LastName == person.LastName).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameLastNameCity:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.LastName == person.LastName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameMiddleName:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.MiddleName == person.MiddleName).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameMiddleNameCity:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.MiddleName == person.MiddleName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameMiddleNameLastName:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.MiddleName == person.MiddleName && x.Person.LastName == person.LastName).ToList();
-                }
-                case PersonExtention.InformationAmount.FirstNameMiddleNameLastNameCity:
-                {
-                    return _collection.Where(x => x.Person.FirstName == person.FirstName && x.Person.MiddleName == person.MiddleName && x.Person.LastName == person.LastName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.LastName:
-                {
-                    return _collection.Where(x => x.Person.LastName == person.LastName).ToList();
-                }
-                case PersonExtention.InformationAmount.LastNameCity:
-                {
-                    return _collection.Where(x => x.Person.LastName == person.LastName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.MiddleName:
-                {
-                    return _collection.Where(x => x.Person.MiddleName == person.MiddleName).ToList();
-                }
-                case PersonExtention.InformationAmount.MiddleNameCity:
-                {
-                    return _collection.Where(x => x.Person.MiddleName == person.MiddleName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.MiddleNameLastName:
-                {
-                    return _collection.Where(x => x.Person.MiddleName == person.MiddleName && x.Person.LastName == person.LastName).ToList();
-                }
-                case PersonExtention.InformationAmount.MiddleNameLastNameCity:
-                {
-                    return _collection.Where(x => x.Person.MiddleName == person.MiddleName && x.Person.LastName == person.LastName && x.ClientLocation.City == city).ToList();
-                }
-                case PersonExtention.InformationAmount.NoInfo:
-                {
-                    throw new Exception();
-                }
+                result = result.Where(x=>x.Person.FirstName==person.FirstName);
             }
-            throw new Exception();
+            if (!string.IsNullOrWhiteSpace(person.MiddleName))
+            {
+                result = result.Where(x => x.Person.MiddleName == person.MiddleName);
+            }
+            if (!string.IsNullOrWhiteSpace(person.LastName))
+            {
+                result = result.Where(x => x.Person.LastName == person.LastName);
+            }
+            if (!string.IsNullOrWhiteSpace(city))
+            {
+                result = result.Where(x => x.ClientLocation.City == city);
+            }
+            return result.ToList();
         }
-
     }
 }
