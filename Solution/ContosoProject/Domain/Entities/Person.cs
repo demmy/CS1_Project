4b0ec28145ffdca35e941a9d4557b25a6f08bf0d
imnikoo <efruev@gmail.com>
2015-10-18 20:15:04 +0300

    person change

--- Solution/ContosoProject/Domain/Entities/Person.cs
+++ Solution/ContosoProject/Domain/Entities/Person.cs
@@ -13,29 +13,5 @@ namespace Domain.Entities
         {
 
         }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Person) obj);
-        }
-
-        protected bool Equals(Person other)
-        {
-            return string.Equals(FirstName, other.FirstName) && string.Equals(MiddleName, other.MiddleName) && string.Equals(LastName, other.LastName);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = (FirstName != null ? FirstName.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (MiddleName != null ? MiddleName.GetHashCode() : 0);
-                hashCode = (hashCode*397) ^ (LastName != null ? LastName.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
     }
 }
