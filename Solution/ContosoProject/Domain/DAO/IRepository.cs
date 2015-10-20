5de8078c4bb60b867104a7450099e8068f579f25
demmy <demiurge.ds@gmail.com>
2015-10-20 13:10:36 +0300

    Modify IRepository

--- Solution/ContosoProject/Domain/DAO/IRepository.cs
+++ Solution/ContosoProject/Domain/DAO/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,9 @@ namespace Domain.DAO
         void Delete(T entity);
         void Delete(int id);
         T Find(int id);
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
 
-        ICollection<T> GetAll();
-        ICollection<T> GetByIsActive(bool isActive);
+        IQueryable<T> GetAll();
+        IQueryable<T> GetByIsActive(bool isActive);
     }
 }
