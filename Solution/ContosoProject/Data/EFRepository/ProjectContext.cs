a2c5119e412dd229870a821898ead9704ad738af
demmy <demiurge.ds@gmail.com>
2015-10-20 12:20:51 +0300

    add entity framework Init context, Configuration and first migration

---
+++ Solution/ContosoProject/Data/EFRepository/ProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Domain.Entities.Orders;
+using Domain.Entities.Products;
+using Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EFRepository
+{
+    public class ProjectContext : DbContext
+    {
+        DbSet<Order> Oders { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Client> Clients { get; set; }
+    }
+}
