350ed356765a2b2874995308e09bf05a6a1c8edc
SalamanderSunburn <taisiya.loza@gmail.com>
2015-10-22 00:21:49 +0300

    merged with NicksForms

---
+++ Solution/ContosoProject/ContosoUI/NicksForms/Client form/ClientViewList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace ContosoUI.NicksForms
+{
+    public partial class clientViewList : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public clientViewList()
+        {
+            InitializeComponent();
+        }
+
+        private void clientViewList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
